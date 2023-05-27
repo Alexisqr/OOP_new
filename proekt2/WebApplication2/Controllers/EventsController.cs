cbf4f88f951b2ded5654796de3a135ef85b717fe
Alexisqr <75916999+Alexisqr@users.noreply.github.com>
2023-05-28 00:00:10 +0300

    Add files via upload

---
+++ proekt2/WebApplication2/Controllers/EventsController.cs
@@ -0,0 +1,197 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShelterEF.DAL.Repositories;
+
+namespace Shelter.DAL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ComentGlobController : ControllerBase
+    {
+       
+        private IUnitOfWorks _UnOfW;
+        private IComentGlobServices _comentGlobServices;
+        public ComentGlobController(
+
+            IUnitOfWorks UnOfW, IComentGlobServices comentGlobServices)
+        {
+            this._UnOfW = UnOfW;
+            this._comentGlobServices = comentGlobServices;
+        }
+        [HttpGet("{id}/all")]
+        public async Task<ActionResult<AnimalsResponceDto>> GetAnimals(int id)
+        {
+            try
+            {
+                var results = await _animalsServices.GetAnimals(id);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //GET: api/events
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animals>>> GetAllEventsAsync()
+        {
+            try
+            {
+                var results = await _ADOuow.AnimalsRepository.GetAllAsync();
+                _ADOuow.Commit();
+                _logger.LogInformation($"Отримали всі івенти з бази даних!");
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //GET: api/events/Id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animals>> GetByIdAsync(int id)
+        {
+            try
+            {
+                var result = await _ADOuow.AnimalsRepository.GetAsync(id);
+                _ADOuow.Commit();
+                if (result == null)
+                {
+                    _logger.LogInformation($"Івент із Id: {id}, не був знайдейний у базі даних");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInformation($"Отримали івент з бази даних!");
+                    return Ok(result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+        //GET: api/events/Id
+        [HttpGet("/EF/{id}")]
+        public async Task<ActionResult<Animals>> GetByIdEFAsync(int id)
+        {
+            try
+            {
+                var result = await _ADOuow.AnimalsRepository.GetAsync(id);
+                _ADOuow.Commit();
+                if (result == null)
+                {
+                    _logger.LogInformation($"Івент із Id: {id}, не був знайдейний у базі даних");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInformation($"Отримали івент з бази даних!");
+                    return Ok(result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //POST: api/events
+        [HttpPost]
+        public async Task<ActionResult> PostEventAsync([FromBody] Animals evnt)
+        {
+            try
+            {
+                if (evnt == null)
+                {
+                    _logger.LogInformation($"Ми отримали пустий json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogInformation($"Ми отримали некоректний json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є некоректним");
+                }
+                var created_id = await _ADOuow.AnimalsRepository.AddAsync(evnt);
+                _ADOuow.Commit();
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі PostEventAsync - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //POST: api/events/id
+        [HttpPut("{id}")]
+        [TypeFilter(typeof(ValidationFilterAttribute))]
+        public async Task<ActionResult> UpdateEventAsync(int id, [FromBody] Animals evnt)
+        {
+            try
+            {
+                if (evnt == null)
+                {
+                    _logger.LogInformation($"Ми отримали пустий json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є null");
+                }
+                if (!ModelState.IsValid)
+                {
+                    _logger.LogInformation($"Ми отримали некоректний json зі сторони клієнта");
+                    return BadRequest("Обєкт івенту є некоректним");
+                }
+
+                var event_entity = await _ADOuow.AnimalsRepository.GetAsync(id);
+                if (event_entity == null)
+                {
+                    _logger.LogInformation($"Івент із Id: {id}, не був знайдейний у базі даних");
+                    return NotFound();
+                }
+
+                await _ADOuow.AnimalsRepository.ReplaceAsync(evnt);
+                _ADOuow.Commit();
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі PostEventAsync - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+        //GET: api/events/Id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteByIdAsync(int id)
+        {
+            try
+            {
+                var event_entity = await _ADOuow.AnimalsRepository.GetAsync(id);
+                if (event_entity == null)
+                {
+                    _logger.LogInformation($"Івент із Id: {id}, не був знайдейний у базі даних");
+                    return NotFound();
+                }
+
+                await _ADOuow.AnimalsRepository.DeleteAsync(id);
+                _ADOuow.Commit();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Транзакція сфейлилась! Щось пішло не так у методі GetAllEventsAsync() - {ex.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, "вот так вот!");
+            }
+        }
+
+    }
+}
---
+++ proekt2/WebApplication2/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using System.Data;
+using System.Data.SqlClient;
+//using Shelter.DAL.Repositories.Interfaces;
+//using Shelter.DAL.Repositories;
+using ShelterEF.DAL.Data;
+using ShelterEF.DAL.Repositories;
+using ShelterDAL.Models;
+//using ShelterBLL.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+//Connection for EF database + DbContext
+
+// Add services to the container.
+//builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+//builder.Configuration.AddJsonFile("appsetings.json", optional: false, reloadOnChange: true);
+//builder.Configuration.AddJsonFile($"appsetings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: false, reloadOnChange: true);
+
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyContext>(options =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("MSSQLConnection");
+    //options.UseSqlServer(connectionString);
+});
+// Connection/Transaction for ADO.NET/DAPPER database
+
+builder.Services.AddScoped((s) => new SqlConnection(builder.Configuration.GetConnectionString("SqlConnection")));
+builder.Services.AddScoped<IDbTransaction>(s =>
+{
+    SqlConnection conn = s.GetRequiredService<SqlConnection>();
+    conn.Open();
+    return conn.BeginTransaction();
+});
+//Connection for EF database + DbContext
+builder.Services.AddScoped<IComentGlobRepository, ComentGlobRepository>();
+builder.Services.AddScoped<IComentGoodRepository, ComentGoodRepository>();
+builder.Services.AddScoped<IComentKindAnimalsRepository, KindOfAnimalsRepository>();
+builder.Services.AddScoped<IUnitOfWorks, UnitOfWorks>();
+
+//builder.Services.AddScoped<IAnimalsRepository, AnimalsRepository>();
+//builder.Services.AddScoped<IKindOfAnimalsRepository, KindOfAnimalsRepository>();
+//builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+//builder.Services.AddScoped<IAnimalsServices, AnimalsServices>();
+// Dependendency Injection for Repositories/UOW from EF DAL
+
+
+
+var app = builder.Build();
+
+
+//////////////////////////////////////////
+// MIDDLEWARE -  ŒÕ¬≈™– Œ¡–Œ¡ » «¿œ»“”
+// Configure the HTTP request pipeline.
+//////////////////////////////////////////
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
